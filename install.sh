58473cb888844c6b2d887edabc5bf9d63b36a424
scvsoft-jjevangelista <161790779+scvsoft-jjevangelista@users.noreply.github.com>
2024-04-02 11:01:19 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-export PS1=\''%{$fg[white]%}%M %{$fg_bold[red]%}➜ %{$fg_bold[green]%}%p %{$fg[cyan]%}%c %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%} % %{$reset_color%}'\'
+export PS1=\''%{$fg_bold[green]%}%p %{$fg[cyan]%}%c %{$fg_bold[blue]%}$(git_prompt_info)%{$fg_bold[blue]%} % %{$reset_color%}'\'
+git config --global submodule.recurse true
