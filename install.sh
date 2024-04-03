9184ada91f8bcab37ed37ed2a949e6c2bbe34e08
scvsoft-jjevangelista <161790779+scvsoft-jjevangelista@users.noreply.github.com>
2024-04-03 11:04:06 -0300

    Delete install.sh

--- install.sh
+++
@@ -1,2 +0,0 @@
-export PS1="\[\033[01;32m\]\t \[\033[01;36m\]\w \[\033[01;34m\]\$(git branch 2>/dev/null | grep '^*' | colrm 1 2) \[\033[01;34m\]% \[\033[0m\]"
-git config --global submodule.recurse true
